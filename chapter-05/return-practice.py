55003285ac3fd4a2771642c42c95f1fcddbbc604
alissamaharaj <alissamaharaj315@gmail.com>
2026-09-14 18:48:27 -0400

    Uploaded pratice file used to understand the return statement in python functions.

---
+++ chapter-05/return-practice.py
@@ -0,0 +1,13 @@
+#def addtwo(a, b):
+#    added = a + b
+#    print(added)
+
+#x =addtwo(3, 5) + 1
+#print(x)
+
+def addtwo(a, b):
+    added = a + b
+    return added
+
+x = addtwo(3, 5) + 1
+print(x)
