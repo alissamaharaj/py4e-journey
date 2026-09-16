f8dca225c03ab1968a96b7a0f0314401a87dbfd4
alissamaharaj <alissamaharaj315@gmail.com>
2026-09-16 18:02:13 -0400

    Uploaded exercise for chapter 6

---
+++ chapter-06/exercises/ex_5_01.py
@@ -0,0 +1,17 @@
+num = 0
+total = 0.0
+while True:
+    sval = input('Enter a number: ')
+    if sval == 'done':
+        break
+    try:
+        fval = float(sval)
+    except:
+        print('Invalid input')
+        continue
+    #print(fval)
+    num = num + 1
+    total = total + fval
+
+#print('ALL DONE')
+print(total, num, total/num)
