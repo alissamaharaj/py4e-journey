0cfec22e1b561a135c404a34fb548d7cbb70f1a2
alissamaharaj <alissamaharaj315@gmail.com>
2026-09-24 19:29:08 -0400

    Uploaded files for chapter 9.

---
+++ chapter-09/exercises/ex_8_00.py
@@ -0,0 +1,9 @@
+fhand = open('mbox.txt')
+
+for line in fhand:
+    line = line.rstrip()
+    words = line.split()
+    # guardian in a compound statement
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    print(words[2])
---
+++ chapter-09/exercises/ex_8_04.py
@@ -0,0 +1,13 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+
+list = list()
+
+for line in fh:
+    words = line.split()
+    for word in words:
+        if word in list: continue
+        list.append(word)
+
+list.sort()
+print(list)
